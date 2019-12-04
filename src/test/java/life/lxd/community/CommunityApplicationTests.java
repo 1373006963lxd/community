078645f63cdaebc4503bd3fa70061232dd930ff4
Your Name <you@example.com>
2019-12-04 12:06:03 +0800

    init repository

---
+++ src/test/java/life/lxd/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.lxd.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
